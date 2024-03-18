7f59fff8e88374d9466bda688c0fa9d00d27f795
Matthew Rodriguez <142768608+mattrodriguez004@users.noreply.github.com>
2024-03-17 20:53:55 -0700

    please

--- js/universal.js
+++ js/universal.js
@@ -12,7 +12,7 @@ function fixNav() {
 }
 
 
-function shuffle(array) {
+/* {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
@@ -36,3 +36,4 @@ const shuffleButton = document.getElementById('shuffleButton');
 if (shuffleButton) {
     shuffleButton.addEventListener('click', shuffleSpotifyTracks);
 }
+*/
