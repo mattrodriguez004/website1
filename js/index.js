2dc3d726edc44826d0f8a1c4efd44337d7c1164a
Matthew Rodriguez <142768608+mattrodriguez004@users.noreply.github.com>
2024-03-18 01:25:57 +0000

    Sun, Mar 17, 2024, 6:25 PM -07:00

--- js/index.js
+++
@@ -1,9 +0,0 @@
-toggle = document.getElementById('toggle')
-open = document.getElementById('open')
-modal = document.getElementById('modal')
-close = document.getElementById('close')
-
-
-toggle.addEventListener('click', () => {
-    document.body.classList.toggle('show-nav')
-})
